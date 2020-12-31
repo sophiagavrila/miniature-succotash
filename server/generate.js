b4e61deb21cc869a6b58449f11cdeec46015a8eb
sophiagavrila <msophiagavrila@gmail.com>
2020-12-31 04:29:25 -0500

    add generate.js file to fake data from faker.js

---
+++ server/generate.js
@@ -0,0 +1,17 @@
+// faker.js is an api used to generate dummy data which I can then pass to an array and display
+
+const faker = require('faker');
+
+let database = { products: [] };
+
+for (let i=0; i<=300; i++) {
+    database.products.push({
+        id: i,
+        name: faker.commerce.productName(),
+        description: faker.lorem.sentances(),
+        price: faker.commerce.price(),
+        imageUrl: "https://source.unsplash.com/user/erondu/1600x900"
+    })
+}
+
+console.log(JSON.stringify(database))
