1a3f98325a7520c7f0a291241d4aa8ccc04731ee
sophiagavrila <msophiagavrila@gmail.com>
2021-01-01 21:09:39 -0500

    confirm SERVER_URL in api service

--- server/generate.js
+++ server/generate.js
@@ -10,7 +10,7 @@ for (let i=0; i<=300; i++) {
         name: faker.commerce.productName(),
         description: faker.lorem.sentences(),
         price: faker.commerce.price(),
-        imageUrl: "https://source.unsplash.com/1600x900/?product",
+        imageUrl: "https://source.unsplash.com/random/300x300",
         quantity: faker.random.number()
     })
 }
